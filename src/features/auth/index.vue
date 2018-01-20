0a03558a54efeb2ee499b497057f0c3219b3c363
Daniel Loureiro Cintra <cintra.70@gmail.com>
2018-01-20 07:35:31 -0200

    first

---
+++ src/features/auth/index.vue
@@ -0,0 +1,11 @@
+<script>
+  export default {
+    name: 'Authentication'
+  }
+</script>
+
+<template>
+	<div>
+		<h1>Sou componente Auth.index</h1>
+	</div>
+</template>
---
+++ src/features/categories/index.vue
@@ -0,0 +1,11 @@
+<script>
+  export default {
+    name: 'Categories'
+  }
+</script>
+
+<template>
+	<div>
+		<h1>Sou componente Categories.index</h1>
+	</div>
+</template>
